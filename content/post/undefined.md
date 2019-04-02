a98d03adfcccd4c652a69013060c01d71a4905c8
ejwillard <ejwillard@squiz.net>
2019-04-02 18:02:50 +1100

    initial commit

---
+++ content/post/undefined.md
@@ -0,0 +1,8 @@
++++
+title = "First Post through CMS"
++++
+
+![](https://www.datocms-assets.com/10914/1554187909-profilepic.png)
+Hey there!
+
+Welcome to the first post on this blog created through the Dato CMS interface!!!! easy editing for clients that's for sure!!!
---
+++ dato.config.js
@@ -0,0 +1,33 @@
+module.exports = (dato, root, i18n) => {
+
+
+    // root.createPost("content/post/my-post.md", "toml", {
+    //     frontmatter: {
+    //         title: "First article",
+    //         type: "post",
+    //         categories: ["random"],
+    //         weight: 4,
+    //         date: "2012-04-06",
+    //     },
+    //     content: "Lorem **ipsum dolor sit amet**, consectetur adipiscing elit."
+    // });
+
+
+    // inside a "src/articles" directory...
+    root.directory("content/post", dir => {
+
+        // ...iterate over the "Blog post" records...
+        dato.posts.forEach((post) => {
+
+            // ...and create a markdown file for each article!
+            dir.createPost(
+                `${post.slug}.md`, "toml", {
+                    frontmatter: {
+                        title: post.title,
+                    },
+                    content: post.content
+                }
+            );
+        });
+    });
+};
