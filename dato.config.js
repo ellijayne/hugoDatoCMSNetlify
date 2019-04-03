2e6462bae7d09e82a80c00d453d60531d223ea76
ejwillard <ejwillard@squiz.net>
2019-04-03 17:01:25 +1100

    changed config again

--- dato.config.js
+++ dato.config.js
@@ -1,27 +1,14 @@
 module.exports = (dato, root, i18n) => {
 
-
-    // root.createPost("content/post/my-post.md", "toml", {
-    //     frontmatter: {
-    //         title: "First article",
-    //         type: "post",
-    //         categories: ["random"],
-    //         weight: 4,
-    //         date: "2012-04-06",
-    //     },
-    //     content: "Lorem **ipsum dolor sit amet**, consectetur adipiscing elit."
-    // });
-
-
     // inside a "src/articles" directory...
-    root.directory("content/post", dir => {
+    root.directory("content/post", (postDir) => {
 
         // ...iterate over the "Blog post" records...
         dato.posts.forEach((post) => {
 
             // ...and create a markdown file for each article!
-            dir.createPost(
-                `${post.slug()}.md`, "toml", {
+            postDir.createPost(
+                `${post.slug}.md`, "yaml", {
                     frontmatter: {
                         title: post.title,
                     },
