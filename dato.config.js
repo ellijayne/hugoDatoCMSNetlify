e1d7e2992a3f2e351ed7fb363cac4d16297ad073
ejwillard <ejwillard@squiz.net>
2019-04-08 12:16:08 +1000

    updated config to hopefully take dato and local posts

--- dato.config.js
+++ dato.config.js
@@ -8,7 +8,19 @@ module.exports = (dato, root, i18n, content) => {
          //dato.posts.forEach((post) => {
             content.posts.forEach((post) => {
 
-           
+                if ((`${post.title}.md`).length = 0) {
+                     dato.posts.forEach((post) => {
+                          postDir.createPost(
+                              `${post.title}.md`, "yaml", {
+                                  frontmatter: {
+                                      title: post.title,
+                                  },
+                                  content: post.content
+                              }
+                          );
+                     })
+                     return;
+                }
 
             // ...and create a markdown file for each article!
             postDir.createPost(
