dd8acf87cc3c3563cd0465b261e049cdf0b7be30
ejwillard <ejwillard@squiz.net>
2019-04-08 12:54:17 +1000

    added two seperate functions in config to pull posts from both places?

--- dato.config.js
+++ dato.config.js
@@ -1,4 +1,4 @@
-module.exports = (dato, root, content, i18n) => {
+module.exports = (dato, root, i18n) => {
 
     // inside a "src/articles" directory...
     root.directory("content/post", (postDir) => {
@@ -16,17 +16,25 @@ module.exports = (dato, root, content, i18n) => {
                 }
             );
         });
-        content.posts.forEach((post) => {
-
-            // ...and create a markdown file for each article!
-            postDir.createPost(
-                `${post.title}.md`, "yaml", {
-                    frontmatter: {
-                        title: post.title,
-                    },
-                    content: post.content
-                }
-            );
-        });
     });
+
+
+
+     root.directory("public/post", (postDir) => {
+
+         // ...iterate over the "Blog post" records...
+         content.posts.forEach((post) => {
+
+             // ...and create a markdown file for each article!
+             postDir.createPost(
+                 `${post.title}.md`, "yaml", {
+                     frontmatter: {
+                         title: post.title,
+                     },
+                     content: post.content
+                 }
+             );
+         });
+     });
+
 };
